885d180b82c624be41a0455ef2c0d05aa2314210
RepeatAl <166618308+RepeatAl@users.noreply.github.com>
2025-04-20 13:53:17 +0200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,57 +1,75 @@
-
 import js from "@eslint/js";
 import globals from "globals";
+import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import react from "eslint-plugin-react";
-import typescript-eslint from "typescript-eslint";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
 
+export default js.FlatESLintConfig.create({
+  // files to ignore
+  ignores: ["dist", "node_modules"],
 
-export default tseslint.config(
-  { ignores: ["dist"] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
+  // base configs to extend
+  extends: [
+    js.configs.recommended,
+    tsPlugin.configs.recommended,
+    react.configs.recommended,
+    reactHooks.configs.recommended,
+  ],
+
+  // only lint TS/TSX and JS/JSX files
+  files: ["**/*.{js,jsx,ts,tsx}"],
+
+  languageOptions: {
+    parser: tsParser,
+    parserOptions: {
       ecmaVersion: 2020,
-      globals: {
-        ...globals.browser,
-        React: "readonly",
-      },
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
+      sourceType: "module",
+      ecmaFeatures: { jsx: true },
     },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-      "react": react,
+    globals: {
+      ...globals.browser,
+      React: "readonly",
     },
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-      "@typescript-eslint/no-unused-vars": ["warn", { 
-        "argsIgnorePattern": "^_",
-        "varsIgnorePattern": "^_" 
-      }],
-      "react/jsx-uses-react": "error",
-      "react/jsx-uses-vars": "error",
-      "react/prop-types": "off",
-      "react/react-in-jsx-scope": "off",
-      "no-console": ["warn", { "allow": ["error", "warn", "info"] }],
-      "prefer-const": "warn",
-      "no-duplicate-imports": "error",
-    },
-  }
-);
+  },
+
+  plugins: {
+    react,
+    "react-hooks": reactHooks,
+    "react-refresh": reactRefresh,
+    "@typescript-eslint": tsPlugin,
+  },
+
+  settings: {
+    react: { version: "detect" },
+  },
+
+  rules: {
+    // React hooks rules
+    ...reactHooks.configs.recommended.rules,
+
+    // React Refresh
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+
+    // TypeScript unused vars
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+    ],
+
+    // React rules
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+
+    // General code style
+    "no-console": ["warn", { allow: ["warn", "error", "info"] }],
+    "prefer-const": "warn",
+    "no-duplicate-imports": "error",
+  },
+});
